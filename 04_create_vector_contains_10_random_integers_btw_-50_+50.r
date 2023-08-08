82088f4155a1b8d0e269f6a9873a78f351d760d3
Shaik Ashfaq <113223950+ShaikAshfaq24056@users.noreply.github.com>
2023-08-08 09:37:36 +0530

    OUTPUT :
    > source("C:/Users/ashfa/OneDrive/Documents/04_create_vector_contains_10_random_integers_btw_-50_+50.r")
    [1] "Content of the vector:"
    [1] "10 random integer values between -50 and +50:"
     [1]  24 -20  19  46 -14  49 -27  18  17 -48

---
+++ 04_create_vector_contains_10_random_integers_btw_-50_+50.r
@@ -0,0 +1,9 @@
+" Write a R program to create a vector which contains 10 random integer values between -50 and 
++50"
+
+v = sample(-50:50, 10, replace=TRUE)
+print("Content of the vector:")
+print("10 random integer values between -50 and +50:")
+print(v)
+
+# by Shaik Ashfaq
