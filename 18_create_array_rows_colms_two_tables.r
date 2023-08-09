81717b737fef292951c550e6cd020b16666194fe
Shaik Ashfaq <113223950+ShaikAshfaq24056@users.noreply.github.com>
2023-08-09 19:50:38 +0530

    OUTPUT :
    > source("~/.active-rstudio-document")
    , , 1
    
         [,1] [,2] [,3]
    [1,]    1    7    6
    [2,]    3    2    8
    [3,]    5    4   10
    
    , , 2
    
         [,1] [,2] [,3]
    [1,]    1    7    6
    [2,]    3    2    8
    [3,]    5    4   10

---
+++ 18_create_array_rows_colms_two_tables.r
@@ -0,0 +1,9 @@
+" Write a R program to create an array with three columns, three rows, and two 'tables', taking 
+two vectors as input to the array. Print the array"
+
+v1 = c(1, 3, 5, 7)
+v2 = c(2, 4, 6, 8, 10)
+arra1 = array(c(v1, v2),dim = c(3,3,2))
+print(arra1)
+
+#by Shaik Ashfaq
