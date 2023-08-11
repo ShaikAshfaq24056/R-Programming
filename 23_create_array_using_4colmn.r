b838713f3df46943c01f1453e7c20e9030f727e5
Shaik Ashfaq <113223950+ShaikAshfaq24056@users.noreply.github.com>
2023-08-11 09:18:11 +0530

    OUTPUT :
    > source("C:/Users/ashfa/OneDrive/Documents/23_create_array_using_4colmn.r")
    , , 1
    
         [,1] [,2] [,3] [,4] [,5]
    [1,]    1    4    7   10   13
    [2,]    2    5    8   11   14
    [3,]    3    6    9   12   15
    
    , , 2
    
         [,1] [,2] [,3] [,4] [,5]
    [1,]   16   19   22   25   28
    [2,]   17   20   23   26   29
    [3,]   18   21   24   27   30

---
+++ 23_create_array_using_4colmn.r
@@ -0,0 +1,7 @@
+"Write a R program to create an array using four given columns, three given rows, and two given 
+tables and display the content of the array"
+
+array1 =  array(1:30, dim=c(3,5,2))
+print(array1)
+
+#by Shaik Ashfaq
